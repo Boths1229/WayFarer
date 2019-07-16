4a4f5eb49450b9e41b4bf45149f21ab901fb75b5
chukwuma Emmanuel <boths1229@gmail.com>
2019-07-16 16:15:09 +0100

    ft(set up): user filter trips
    set up for trip filtering
    
    [finishes #167301348]

--- server/middlewares/isAdmin.js
+++ server/middlewares/isAdmin.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const isAdmin = (req, res, next) => {
   // const token = req.headers.authorization || req.params.token || req.headers['x-access-token'] || req.body.token;
-  const token = req.headers.token || req.body.token;
+  const token =  req.headers['x-access-token'] || req.headers.token || req.body.token;
   if (!token) {
     return res.status(403).json({
       status: 'error',
