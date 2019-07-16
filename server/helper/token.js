e7ec209c020553e4990f30805c231fb28bbc02ac
chukwuma Emmanuel <boths1229@gmail.com>
2019-07-16 15:54:58 +0100

    ft(set up): user filter trips
    set up for trip filtering
    
    [finishes #167301348]

--- server/helper/token.js
+++ server/helper/token.js
@@ -8,7 +8,7 @@ export const createToken = (data) => {
 
 export const verifyToken = (req, res, next) => {
   // const token = req.headers.authorization || req.params.token || req.headers['x-access-token'] || req.body.token;
-  const token = req.headers.token;
+  const token = req.headers.token || req.body.token;
   
   if (!token) {
     return res.status(403).json({
